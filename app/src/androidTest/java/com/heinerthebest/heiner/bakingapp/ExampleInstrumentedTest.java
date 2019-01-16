46267db018c16491a784eae2178923cce4860776
heiner <heinerbauza@gmail.com>
2019-01-15 22:02:36 -0500

    apply test for check the fragment

--- app/src/androidTest/java/com/heinerthebest/heiner/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/heinerthebest/heiner/bakingapp/ExampleInstrumentedTest.java
@@ -23,4 +23,5 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.heinerthebest.heiner.bakingapp", appContext.getPackageName());
     }
+
 }
---
+++ app/src/androidTest/java/com/heinerthebest/heiner/bakingapp/MainActivityChangeTitleInRecipeTest.java
@@ -0,0 +1,44 @@
+package com.heinerthebest.heiner.bakingapp;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.heinerthebest.heiner.bakingapp.Activities.DescriptionActivity;
+import com.heinerthebest.heiner.bakingapp.Fragments.IngredientsFragment;
+import com.heinerthebest.heiner.bakingapp.Fragments.StepFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityChangeTitleInRecipeTest {
+
+    @Rule
+    public ActivityTestRule<DescriptionActivity> activityActivityTestRule = new ActivityTestRule<DescriptionActivity>(DescriptionActivity.class);
+
+
+    @Before
+    public void init(){
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+
+    @Test
+    public void checkShowHeadContainerWithIngredients() {
+        IngredientsFragment fragment = new IngredientsFragment();
+        activityActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction().add(R.id.head_container, fragment).commit();
+    }
+
+    @Test
+    public void checkShowBodyContainerWithSteps() {
+        StepFragment fragment = new StepFragment();
+        activityActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction().add(R.id.body_container, fragment).commit();
+    }
+
+
+}
